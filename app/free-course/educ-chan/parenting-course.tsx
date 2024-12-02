452ef8c47a0b4c7159303b44431af5e2bdf36688
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2024-12-03 00:15:28 +0200

    Starting profile screen

---
+++ app/free-course/educ-chan/parenting-course.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ParentingCourse = () => {
+  return (
+    <View>
+      <Text>ParentingCourse</Text>
+    </View>
+  )
+}
+
+export default ParentingCourse
+
+const styles = StyleSheet.create({})
